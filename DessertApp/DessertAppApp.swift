e5790e2b5a5f94704a2e143cb22c655030fd3348
UgurcanCirak <cengugurcancirak@gmail.com>
2025-05-30 23:10:23 +0300

    Initial Commit

---
+++ DessertApp/DessertAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertAppApp.swift
+//  DessertApp
+//
+//  Created by Uğurcan Çırak on 30.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct DessertAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
